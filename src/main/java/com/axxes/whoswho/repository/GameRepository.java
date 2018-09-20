6ed75c1a3ad8215b4b070392bc713e2ea5174491
Jelle De Vleminck <Jelle.devleminck@axxes.com>
2018-09-20 10:23:35 +0200

    scoreboard repository fucntion

--- src/main/java/com/axxes/whoswho/repository/GameRepository.java
+++ src/main/java/com/axxes/whoswho/repository/GameRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface GameRepository extends CrudRepository<Game, Long> {
     List<Game> findByEndTimeBetween(LocalDate start, LocalDate end);
